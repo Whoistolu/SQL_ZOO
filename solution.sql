4c0f098d8dffca9631f4ef6515530a3d5ce0f319
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-24 22:59:35 +0100

    add solution

--- solution.sql
+++ solution.sql
@@ -233,3 +233,26 @@ WHERE continent = 'Africa';
 SELECT COUNT(area)
 FROM world
 WHERE area > 1000000;
+
+
+-- 5. Baltic states population
+SELECT SUM(population)
+FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+-- 6. Counting the countries of each continent
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+
+-- 7. Counting big countries in each continent
+SELECT continent, COUNT(name)
+FROM world
+WHERE population > 10000000
+GROUP BY continent;
+
+-- 8. Counting big continents
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) > 100000000;
