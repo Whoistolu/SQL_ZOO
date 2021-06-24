ffb307ee1015177096eb7caeeb2b06c1721fbf1e
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-24 23:02:14 +0100

    add solution

--- solution.sql
+++ solution.sql
@@ -421,3 +421,35 @@ WHERE movieid IN (
   SELECT movieid FROM casting WHERE actorid IN (
   SELECT id FROM actor
   WHERE name='Art Garfunkel')) AND name <> 'Art Garfunkel';
+
+  -- Using Null
+
+-- 1. NULL, INNER JOIN, LEFT JOIN, RIGHT JOIN
+SELECT name FROM teacher WHERE dept IS NULL;
+
+-- 2.
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id);
+
+-- 3.
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id);
+
+-- 4.
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id);
+
+-- 5.
+SELECT teacher.name, COALESCE(mobile, '07986 444 2266') FROM teacher;
+
+-- 6.
+SELECT teacher.name, COALESCE(dept.name, 'None') FROM teacher LEFT JOIN dept ON teacher.dept = dept.id;
+
+-- 7.
+SELECT COUNT(teacher.id), COUNT(mobile) FROM teacher;
+
+-- 8.
+SELECT dept.name, COUNT(teacher.dept) FROM teacher RIGHT JOIN dept ON teacher.dept = dept.id GROUP BY dept.name;
