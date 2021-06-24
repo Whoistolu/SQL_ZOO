8ca5f6af9c8a3033ebbc9a1e06f50ebd182b3707
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-24 23:03:46 +0100

    add solution

--- solution.sql
+++ solution.sql
@@ -503,3 +503,29 @@ SELECT DISTINCT a.company, a.num
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop=115 AND b.stop = 137;
+
+-- 8.
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'Tollcross';
+
+-- 9.
+SELECT DISTINCT stopb.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart';
+
+-- 10.
+SELECT a.num, a.company,  stops.name,  d.num, d.company
+FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+JOIN stops ON b.stop = stops.id
+JOIN route c ON c.stop = stops.id
+JOIN route d ON c.company = d.company AND c.num = d.num
+WHERE a.stop = (SELECT id FROM stops WHERE name = 'Craiglockhart')
+AND d.stop = (SELECT id FROM stops WHERE name = 'Lochend')
+ORDER BY a.num, stops.name, d.num;
