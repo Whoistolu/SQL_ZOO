27a855ed4e74eef9858a019b283c46726e3cd5e0
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-24 22:57:35 +0100

    add solution

--- solution.sql
+++ solution.sql
@@ -171,3 +171,21 @@ WHERE continent IN (SELECT continent
   FROM world
   WHERE name IN ('Argentina', 'Australia'))
   ORDER BY name;
+
+  -- 4. Between Canada and Poland
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world 
+  WHERE name = 'Canada') AND population < (SELECT population FROM world 
+    WHERE name = 'Poland');
+
+-- 5. Percentages of Germany
+SELECT name, CONCAT(ROUND(100*population/
+  (SELECT population FROM world WHERE name = 'Germany')), '%') AS percentage
+FROM world
+WHERE continent = 'Europe';
+
+-- 6. Bigger than every country in Europe
+SELECT name FROM world 
+  WHERE gdp > (SELECT MAX(gdp) FROM world 
+    WHERE continent = 'Europe');
