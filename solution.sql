5d3c2c2bc319e47e0df8f0910749c7d4081d7057
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-24 23:01:21 +0100

    add solution

--- solution.sql
+++ solution.sql
@@ -327,3 +327,50 @@ SELECT mdate, team1,
   SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
   FROM game LEFT JOIN goal ON matchid = id 
   GROUP BY mdate, matchid, team1, team2;
+
+  -- More JOIN operations
+
+-- 1. 1962 movies
+SELECT id, title
+ FROM movie
+ WHERE yr=1962;
+
+-- 2. When was Citizen Kane released?
+SELECT yr
+ FROM movie
+ WHERE title = 'Citizen Kane';
+
+-- 3. Star Trek movies
+SELECT id, title, yr
+ FROM movie
+ WHERE title LIKE '%Star Trek%'
+ORDER BY yr;
+
+-- 4. id for actor Glenn Close
+SELECT id FROM actor WHERE name = 'Glenn Close';
+
+-- 5. id for Casablanca
+SELECT id FROM movie WHERE title = 'Casablanca';
+
+-- 6. Cast list for Casablanca
+SELECT name FROM actor 
+JOIN casting ON id = actorid
+WHERE movieid=11768;
+
+-- 7. Alien cast list
+SELECT name FROM actor 
+JOIN casting ON actor.id = actorid
+JOIN movie ON movie.id = movieid
+WHERE title= 'Alien';
+
+-- 8. Harrison Ford movies
+SELECT title FROM movie 
+JOIN casting ON movie.id = movieid
+JOIN actor ON actor.id = actorid
+WHERE name= 'Harrison Ford';
+
+-- 9. Harrison Ford as a supporting actor
+SELECT title FROM movie 
+JOIN casting ON movie.id = movieid
+JOIN actor ON actor.id = actorid
+WHERE name= 'Harrison Ford' AND ord<>1;
